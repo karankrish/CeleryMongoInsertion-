f9a054113d3114d6b3b0bd8770d5b448906ed3d7
Karan <mail2karankrishnan@gmail.com>
2019-10-01 17:22:24 +0530

    try catch implemented

--- pgm.py
+++ pgm.py
@@ -3,6 +3,18 @@ from flask_celery import make_celery
 from pymongo import MongoClient
 import json
 from flask_cors import CORS
+import logging
+from logging.handlers import RotatingFileHandler
+
+
+logging.basicConfig(filename='log/api.log', level=logging.ERROR, 
+                    format='%(asctime)s %(levelname)s %(name)s %(message)s')
+logger=logging.getLogger("dnslog.log")
+handler = RotatingFileHandler("log/dnslog.log", maxBytes=2000, backupCount=25) 
+if not logger: 
+    logger.addHandler(handler)
+    
+    
 
 with open('config.json') as f:
     config = json.load(f)
@@ -15,22 +27,30 @@ celery = make_celery(app)
 
 @app.route('/', methods=['POST'])
 def process():
-    data = request.json
-    function.delay(data)
-    return "ok"
+    try:
+        data = request.json
+        function.delay(data)
+        return "ok"
+    except Exception as e:
+        logger.error("------flask api---------" +str(e))
+        return "failed"
 @celery.task(name="pgm.function")
 def function(data):
-  '''
+    try:
+        '''
     client =MongoClient(config['mongodb']['host'],
                                        username=config['mongodb']['username'],
                                        password=config['mongodb']['password'],
                                        authSource=config['mongodb']['authSource'])
-  '''
-  client =MongoClient(config['mongodb']['host'])
-  db = client.DomainMonitor
-  collection = db.api
-  collection.insert(data)
-  return "completed"
+        '''
+  
+        client =MongoClient(config['mongodb']['host'])
+        db = client.DomainMonitor
+        collection = db.api
+        collection.insert(data)
+    except Exception as e:
+        logger.error("------DB---------" +str(e))
+    return "completed"
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
