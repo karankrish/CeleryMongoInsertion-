317b869966329a294445742ca59eeef15d7c2fc1
Karan <mail2karankrishnan@gmail.com>
2019-10-12 16:08:36 +0530

    mysql  db connection

--- send.py
+++ send.py
@@ -1,4 +1,6 @@
 import pika
+import mysql.connector
+
 def SendUrl(data):
     parameters = pika.URLParameters('amqp://user:bitnami@localhost:5672/')
     connection = pika.BlockingConnection(parameters)
@@ -7,3 +9,21 @@ def SendUrl(data):
     channel.basic_publish(exchange='', routing_key='url', body=str(data))
     connection.close()
     return 
+
+def mysqlread():
+    mydb = mysql.connector.connect(
+      host="35.184.228.174",
+      user="root",
+      passwd="admin",
+      database="wcag"
+    )
+    
+    mycursor = mydb.cursor()
+    
+    mycursor.execute("SELECT parseUrl FROM parseurltable;")
+    
+    myresult = mycursor.fetchall()
+    a = []
+    for i in myresult:
+        a.append(i[0])
+    return a
